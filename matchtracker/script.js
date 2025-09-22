3534a710e131d8245fdc7969cf762213dbf4b37e
kayyerix <frederickraynard@gmail.com>
2025-09-22 16:46:17 +0700

    Update script.js

--- matchtracker/script.js
+++ matchtracker/script.js
@@ -14,6 +14,65 @@ document.getElementById("matchBtn").addEventListener("click", function() {
     return;
   }
 
+  if (
+    (name1.toLowerCase() === "raynard" && name2.toLowerCase() === "tiffany") ||
+    (name1.toLowerCase() === "tiffany" && name2.toLowerCase() === "raynard")
+  ) {
+    result.textContent = `❤️ Kecocokan ${name1} & ${name2} adalah 0%!`;
+    return;
+  }
+
+  if (
+    (name1.toLowerCase() === "jason" && name2.toLowerCase() === "vanessa") ||
+    (name1.toLowerCase() === "vanessa" && name2.toLowerCase() === "jason")
+  ) {
+    result.textContent = `❤️ Kecocokan ${name1} & ${name2} adalah 100%!`;
+    return;
+  }
+
+  if (
+    (name1.toLowerCase() === "freddy" && name2.toLowerCase() === "davina") ||
+    (name1.toLowerCase() === "davina" && name2.toLowerCase() === "freddy")
+  ) {
+    result.textContent = `❤️ Kecocokan ${name1} & ${name2} adalah 100%!`;
+    return;
+  }
+
+  if (
+    (name1.toLowerCase() === "peter" && name2.toLowerCase() === "chika") ||
+    (name1.toLowerCase() === "chika" && name2.toLowerCase() === "peter")
+  ) {
+    result.textContent = `❤️ Kecocokan ${name1} & ${name2} adalah 100%!`;
+    return;
+  }
+
+  if (
+    (name1.toLowerCase() === "raynard" && name2.toLowerCase() === "nathania") ||
+    (name1.toLowerCase() === "nathania" && name2.toLowerCase() === "raynard")
+  ) {
+    result.textContent = `❤️ Kecocokan ${name1} & ${name2} adalah 0%!`;
+    return;
+  }
+
+  if (
+    (name1.toLowerCase() === "raynard" && name2.toLowerCase() === "thania") ||
+    (name1.toLowerCase() === "thania" && name2.toLowerCase() === "raynard")
+  ) {
+    result.textContent = `❤️ Kecocokan ${name1} & ${name2} adalah 0%!`;
+    return;
+  }
+
+  if (
+    (name1.toLowerCase() === "raynard" && name2.toLowerCase() === "tania") ||
+    (name1.toLowerCase() === "tania" && name2.toLowerCase() === "raynard")
+  ) {
+    result.textContent = `❤️ Kecocokan ${name1} & ${name2} adalah 0%!`;
+    return;
+  }
+
+
+
+
   // Algoritma cocok-cocokan
   const combined = (name1 + name2).toLowerCase().replace(/\s+/g, "");
   let total = 0;
